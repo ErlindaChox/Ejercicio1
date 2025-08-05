8e8ec1dd9910564ec1f0f2c7fdb12f800c44a98c
ErlindaChox <erlindachox1@gmail.com>
2025-08-04 22:47:57 -0600

    ejercicio2

---
+++ Class3.cs
@@ -0,0 +1,38 @@
+﻿public class Vehiculo
+{
+    private bool encendido = false;  // Por defecto está apagado
+
+    // Propiedad Encendido (solo lectura)
+    public bool Encendido
+    {
+        get { return encendido; }
+    }
+
+    // Método para encender el vehículo
+    public void Encender()
+    {
+        if (!encendido)
+        {
+            encendido = true;
+            Console.WriteLine("Vehículo encendido.");
+        }
+        else
+        {
+            Console.WriteLine("El vehículo ya está encendido.");
+        }
+    }
+
+    // Método para apagar el vehículo
+    public void Apagar()
+    {
+        if (encendido)
+        {
+            encendido = false;
+            Console.WriteLine("Vehículo apagado.");
+        }
+        else
+        {
+            Console.WriteLine("El vehículo ya está apagado.");
+        }
+    }
+}
